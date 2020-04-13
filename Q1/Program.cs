bcb5987b7b8176ff1d98c6319ada8b774366b6f5
Fabricio Henrique <fabriciohod@hotmail.com>
2020-04-13 11:24:23 -0300

    Q1 correção

--- Q1/Program.cs
+++ Q1/Program.cs
@@ -8,7 +8,7 @@ namespace Q1
         static void Main (string[] args)
         {
             int[, ] matriz = new int[3, 3];
-            matriz = PreecherMatriz (matriz);
+            matriz = PreecherMatrizUsiario (matriz);
             ImprimirMatriz (matriz);
             Console.WriteLine ($"\nA soma dos numeros Impares da Matriz: {FiltroParaMatiz(n => n % 2 == 1, matriz)}");
 
@@ -25,7 +25,21 @@ namespace Q1
             }
             return sum;
         }
-        static int[, ] PreecherMatriz (int[, ] matriz)
+        static int[, ] PreecherMatrizUsiario (int[, ] matriz)
+        {
+            Random rnd = new Random ();
+            for (int i = 0; i < matriz.GetLength (0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength (1); j++)
+                {
+                    Console.WriteLine ($"Digite um numro para a posicao {matriz[i,j]} ");
+                    matriz[i, j] = int.Parse (Console.ReadLine ());
+                }
+            }
+            return matriz;
+        }
+        // Estava usando essa para testar
+        static int[, ] PreecherMatrizAutomatico (int[, ] matriz)
         {
             Random rnd = new Random ();
             for (int i = 0; i < matriz.GetLength (0); i++)
@@ -39,6 +53,7 @@ namespace Q1
         }
         static void ImprimirMatriz (int[, ] matriz)
         {
+            System.Console.WriteLine (" Matriz:");
             for (int i = 0; i < matriz.GetLength (0); i++)
             {
                 for (int j = 0; j < matriz.GetLength (1); j++)
