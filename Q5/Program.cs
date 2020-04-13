6e101ba08289074c62b37e8abbb8d7cbeaef555c
Fabricio Henrique <fabriciohod@hotmail.com>
2020-04-13 14:19:57 -0300

    Q5

--- Q5/Program.cs
+++ Q5/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace Q5
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = { 1, 1, 2, 3, 1, 4, 4 };
+            Dictionary<int, int> NumerosRepetidos = new Dictionary<int, int> ();
+            foreach (var item in array)
+            {
+                if (array.GroupBy (x => x).Any (g => g.Count () > 1) && !NumerosRepetidos.ContainsKey (item)) NumerosRepetidos.Add (item, 0);
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (NumerosRepetidos.ContainsKey (array[i])) NumerosRepetidos[array[i]] += 1;
+            }
+            var maxValue = NumerosRepetidos.Values.Max ();
+            foreach (var item in NumerosRepetidos)
+            {
+                if (maxValue == item.Value)
+                {
+                    for (int i = 0; i < item.Value; i++)
+                    {
+                        Console.Write ($" {item.Key} ");
+                    }
+                }
+            }
+            System.Console.WriteLine ();
         }
     }
 }
