f1f4a9108206a60d2a8acffd429c7c6e472bd108
Fabricio Henrique <fabriciohod@hotmail.com>
2020-04-13 12:32:31 -0300

    Q4

---
+++ Q3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace q3
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int[, ] matriz = new int[3, 3];
+            (int max, int min) resultado = (0, 0);
+            matriz = PreecherMatrizAutomatico (matriz);
+            resultado = MaxAndMinMatriz (matriz);
+            ImprimirMatriz (matriz);
+            Console.WriteLine ($"\nO maior valor {resultado.max}, o menor valor e {resultado.min}");
+        }
+        static (int, int) MaxAndMinMatriz (int[, ] matriz)
+        {
+            int max = 0;
+            int min = 0;
+            foreach (var item in matriz)
+            {
+                if (item > max)
+                {
+                    max = item;
+                    min = max;
+                }
+                if (min >= item) min = item;
+            }
+            return (max, min);
+        }
+        static int[, ] PreecherMatrizAutomatico (int[, ] matriz)
+        {
+            Random rnd = new Random ();
+            for (int i = 0; i < matriz.GetLength (0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength (1); j++)
+                {
+                    matriz[i, j] = rnd.Next (1, 10);
+                }
+            }
+            return matriz;
+        }
+        static void ImprimirMatriz (int[, ] matriz)
+        {
+            System.Console.WriteLine (" Matriz:");
+            for (int i = 0; i < matriz.GetLength (0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength (1); j++)
+                {
+                    Console.Write ($" {matriz[i,j] }");
+                }
+                Console.WriteLine ();
+            }
+        }
+    }
+}
--- Q4/Program.cs
+++ Q4/Program.cs
@@ -4,9 +4,41 @@ namespace Q4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine ("Digite o valor de linhas: ");
+            int i = int.Parse (Console.ReadLine ());
+            Console.WriteLine ("Digite o valor de colunas: ");
+            int j = int.Parse (Console.ReadLine ());
+            int[, ] matriz = new int[i, j];
+            matriz = QtdDeCasasNaMatriz (matriz);
+            ImprimirMatriz (matriz);
+        }
+        static int[, ] QtdDeCasasNaMatriz (int[, ] matriz)
+        {
+            int sum = 0;
+            if (matriz.GetLength (0) != matriz.GetLength (1)) throw new ArgumentException ("Digite uma matriz quadrada");
+            for (int i = 0; i < matriz.GetLength (1); i++)
+            {
+                for (int j = 0; j < matriz.GetLength (0); j++)
+                {
+                    sum++;
+                    matriz[j, i] = sum;
+                }
+            }
+            return matriz;
+        }
+        static void ImprimirMatriz (int[, ] matriz)
+        {
+            System.Console.WriteLine (" Matriz:");
+            for (int i = 0; i < matriz.GetLength (0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength (1); j++)
+                {
+                    Console.Write ($" |{matriz[i,j] }|");
+                }
+                Console.WriteLine ();
+            }
         }
     }
 }
